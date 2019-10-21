4bf9d07e195474c11802581fc3ca44f1b9bdbeaf
Julian Berman <Julian@GrayVines.com>
2019-10-21 18:48:46 -0400

    Codecov.io is super flaky.

--- docs/conf.py
+++ docs/conf.py
@@ -6,15 +6,7 @@
 # full list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath("."))
+import re
 
 
 # -- Project information -----------------------------------------------------
@@ -204,3 +196,12 @@ epub_exclude_files = ["search.html"]
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {"https://docs.python.org/": None}
+
+# -- Options for the linkcheck builder ------------------------------------
+
+
+def entire_domain(host):
+    return r"http.?://" + re.escape(host) + r"($|/.*)"
+
+
+linkcheck_ignore = [entire_domain("codecov.io")]
