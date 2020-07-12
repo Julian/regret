Julian/regret

commit fa2cee62c28790664c43ac6cdc073d8b667f4038

Fix wrong stack levels for warnings.
It's always good when I'm bitten by my own filed bugs...

This one is https://twistedmatrix.com/trac/ticket/9363

regret/_warnings.py
@@ -4,7 +4,7 @@ Integration with the standard library's ``warnings`` module.
 import warnings
 
 # as everyone well knows, 6 is the best number.
-_STACKLEVELS_UNTIL_EMIT_IS_CALLED = 6
+_STACKLEVELS_UNTIL_EMIT_IS_CALLED = 4
 
 
 def emit(deprecation):

regret/tests/test_integration.py
@@ -1,4 +1,5 @@
 from datetime import date
+import sys
 
 from twisted.trial.unittest import SynchronousTestCase
 
@@ -40,13 +41,14 @@ def divide():
 
 
 class TestRegret(SynchronousTestCase):
-    def assertDeprecated(self, message, filename, fn, args=(), kwargs={}):
+    def assertDeprecated(self, message, fn, args=(), kwargs={}):
         # Sigh... assertWarns takes positional args positionally, instead of as
         # a sequence.
         return self.assertWarns(
             DeprecationWarning,
             message,
-            filename,
+            # sigh, see https://twistedmatrix.com/trac/ticket/9363
+            sys.modules[self.assertWarns.__module__].__file__,
             fn,
             *args,
             **kwargs
@@ -55,7 +57,6 @@ class TestRegret(SynchronousTestCase):
     def test_function(self):
         result = self.assertDeprecated(
             message="calculate is deprecated.",
-            filename=__file__,
             fn=calculate,
         )
         self.assertEqual(result, 12)
@@ -63,7 +64,6 @@ class TestRegret(SynchronousTestCase):
     def test_function_with_args(self):
         result = self.assertDeprecated(
             message="add is deprecated.",
-            filename=__file__,
             fn=add,
             args=(9,),
             kwargs=dict(y=3),
@@ -76,7 +76,6 @@ class TestRegret(SynchronousTestCase):
                 "calculator_fn is deprecated. "
                 "Please use Calculator instead."
             ),
-            filename=__file__,
             fn=calculator_fn,
         )
         self.assertEqual(result, 9)
@@ -85,7 +84,6 @@ class TestRegret(SynchronousTestCase):
         calculator = Calculator()
         result = self.assertDeprecated(
             message="Calculator.calculate is deprecated.",
-            filename=__file__,
             fn=calculator.calculate,
         )
         self.assertEqual(result, 12)
@@ -94,7 +92,6 @@ class TestRegret(SynchronousTestCase):
         calculator = Calculator()
         result = self.assertDeprecated(
             message="Calculator.__call__ is deprecated.",
-            filename=__file__,
             fn=calculator,
         )
         self.assertEqual(result, 12)
@@ -110,7 +107,6 @@ class TestRegret(SynchronousTestCase):
 
         self.assertDeprecated(
             message="etaluclac is deprecated.",
-            filename=__file__,
             fn=calculate,
         )
 
@@ -161,7 +157,6 @@ class TestRegret(SynchronousTestCase):
                 "divide is deprecated. "
                 "Division is also terrible and we should all be friends."
             ),
-            filename=__file__,
             fn=deprecated,
         )
 
@@ -177,7 +172,6 @@ class TestRegret(SynchronousTestCase):
                 "divide is deprecated. Please use Calculator instead. "
                 "Division is also terrible and we should all be friends."
             ),
-            filename=__file__,
             fn=deprecated,
         )
 
@@ -192,7 +186,6 @@ class TestRegret(SynchronousTestCase):
                 "divide is deprecated. "
                 "It will be removed on or after 2012-12-12."
             ),
-            filename=__file__,
             fn=deprecated,
         )
 
@@ -209,7 +202,6 @@ class TestRegret(SynchronousTestCase):
                 "It will be removed on or after 2012-12-12. "
                 "Please use Calculator instead."
             ),
-            filename=__file__,
             fn=deprecated,
         )
 
@@ -226,7 +218,6 @@ class TestRegret(SynchronousTestCase):
                 "It will be removed on or after 2012-12-12. "
                 "Division is also terrible and we should all be friends."
             ),
-            filename=__file__,
             fn=deprecated,
         )
 
@@ -245,7 +236,6 @@ class TestRegret(SynchronousTestCase):
                 "Please use Calculator instead. "
                 "Division is also terrible and we should all be friends."
             ),
-            filename=__file__,
             fn=deprecated,
         )
 
@@ -256,6 +246,5 @@ class TestRegret(SynchronousTestCase):
 
         self.assertDeprecated(
             message="Subclassing from Calculator is deprecated.",
-            filename=__file__,
             fn=subclass,
         )

commit de2b4286f0375e7c63adb86df055af425e7c3c06

Fix more wrong stack levels.
Rest of the fallout from fa2cee6 and / or
https://twistedmatrix.com/trac/ticket/9363.

Here, __file__ *is* correct, because we're
wrapping the inheritance in a function, so it
*will* in fact happen in the test_integration
file and not in assertWarns' defining module.

Hooray...

(I guess I'm at least happy to kill the extra
complexity around deprecations being able to
add extra stack levels to remove. It's not
needed, at least not yet.)

regret/_warnings.py
@@ -3,7 +3,7 @@ Integration with the standard library's ``warnings`` module.
 """
 import warnings
 
-# as everyone well knows, 6 is the best number.
+
 _STACKLEVELS_UNTIL_EMIT_IS_CALLED = 4
 
 
@@ -11,9 +11,5 @@ def emit(deprecation):
     warnings.warn(
         deprecation.message(),
         DeprecationWarning,
-        stacklevel=_stacklevel_for(deprecation),
+        stacklevel=_STACKLEVELS_UNTIL_EMIT_IS_CALLED,
     )
-
-
-def _stacklevel_for(deprecation):
-    return _STACKLEVELS_UNTIL_EMIT_IS_CALLED + deprecation.stacklevels_added()

regret/emitted.py
@@ -17,9 +17,6 @@ class Deprecation(object):
     _removal_date = attr.ib(default=None, repr=False)
     _addendum = attr.ib(default=None, repr=False)
 
-    def stacklevels_added(self):
-        return getattr(self._kind, "stacklevels_added", int)()
-
     def message(self):
         parts = [self._kind.message(name_of=self._name_of)]
         if self._removal_date is not None:
@@ -58,9 +55,6 @@ class Inheritance(object):
     """
 
     _type = attr.ib()
-
-    def stacklevels_added(self):
-        return 1  # __init_subclass__
 
     def message(self, name_of):
         return "Subclassing from {} is deprecated.".format(

regret/tests/test_integration.py
@@ -41,14 +41,25 @@ def divide():
 
 
 class TestRegret(SynchronousTestCase):
-    def assertDeprecated(self, message, fn, args=(), kwargs={}):
-        # Sigh... assertWarns takes positional args positionally, instead of as
-        # a sequence.
+    def assertDeprecated(
+        self,
+        message,
+        fn,
+        *,
+        filename=None,
+        args=(),
+        kwargs={},
+    ):
+        if filename is None:
+            # sigh, see https://twistedmatrix.com/trac/ticket/9363
+            filename = sys.modules[self.assertWarns.__module__].__file__
+
+        # Sigh... assertWarns takes positional args positionally,
+        # instead of as a sequence.
         return self.assertWarns(
             DeprecationWarning,
             message,
-            # sigh, see https://twistedmatrix.com/trac/ticket/9363
-            sys.modules[self.assertWarns.__module__].__file__,
+            filename,
             fn,
             *args,
             **kwargs
@@ -246,5 +257,6 @@ class TestRegret(SynchronousTestCase):
 
         self.assertDeprecated(
             message="Subclassing from Calculator is deprecated.",
+            filename=__file__,
             fn=subclass,
         )