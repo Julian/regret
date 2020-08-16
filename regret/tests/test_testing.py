426aa134b176b0eb00ddb62b8c5be5c6d76946ec
Julian Berman <Julian@GrayVines.com>
2020-08-15 23:21:42 -0400

    Quick set of integration tests for the testing helper.

---
+++ regret/tests/test_testing.py
@@ -0,0 +1,65 @@
+"""
+Integration tests for the testing helper(s).
+"""
+from unittest import TestCase
+
+from regret import Deprecator, emitted, testing
+
+
+def calculate():
+    return 12
+
+
+class TestRecorder(TestCase):
+    def test_it_sees_emitted_deprecations(self):
+        recorder = testing.Recorder()
+        regret = Deprecator(emit=recorder.emit)
+
+        deprecated = regret.callable(version="1.2.3")(calculate)
+
+        with recorder.expect(
+            emitted.Deprecation(kind=emitted.Callable(object=calculate)),
+            emitted.Deprecation(kind=emitted.Callable(object=calculate)),
+        ):
+            deprecated()
+            deprecated()
+
+    def test_it_errors_for_missing_deprecations(self):
+        recorder = testing.Recorder()
+        regret = Deprecator(emit=recorder.emit)
+
+        deprecated = regret.callable(version="1.2.3")(calculate)
+
+        with self.assertRaises(testing.ExpectedDifferentDeprecations):
+            with recorder.expect(
+                emitted.Deprecation(kind=emitted.Callable(object=calculate)),
+                emitted.Deprecation(kind=emitted.Callable(object=calculate)),
+            ):
+                deprecated()
+
+    def test_it_errors_for_extra_deprecations(self):
+        recorder = testing.Recorder()
+        regret = Deprecator(emit=recorder.emit)
+
+        deprecated = regret.callable(version="1.2.3")(calculate)
+
+        with self.assertRaises(testing.ExpectedDifferentDeprecations):
+            with recorder.expect(
+                emitted.Deprecation(kind=emitted.Callable(object=calculate)),
+                emitted.Deprecation(kind=emitted.Callable(object=calculate)),
+            ):
+                deprecated()
+                deprecated()
+                deprecated()
+
+    def test_it_errors_for_non_matching_deprecations(self):
+        recorder = testing.Recorder()
+        regret = Deprecator(emit=recorder.emit)
+
+        deprecated = regret.callable(version="1.2.3")(calculate)
+
+        with self.assertRaises(testing.ExpectedDifferentDeprecations):
+            with recorder.expect(
+                emitted.Deprecation(kind=emitted.Callable(object=int)),
+            ):
+                deprecated()
