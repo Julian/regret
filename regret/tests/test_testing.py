229ade7c7a1690931bbf09dc292f9d8dc036cb44
Adi Roiban <adi.roiban@chevah.com>
2020-12-06 13:49:23 +0000

    Initial proof of concept.

--- regret/tests/test_testing.py
+++ regret/tests/test_testing.py
@@ -1,13 +1,19 @@
 """
 Integration tests for the testing helper(s).
 """
+import warnings
+
 from unittest import TestCase
 
+import regret
 from regret import Deprecator, emitted, testing
 
 
 def calculate():
     return 12
+
+def replacement_calculate():
+    return 11
 
 
 class TestRecorder(TestCase):
@@ -108,3 +114,112 @@ class TestRecorder(TestCase):
         with self.assertRaises(testing.ExpectedDifferentDeprecations):
             with recorder.expect_clean():
                 deprecated()
+
+
+class TestWarnigsCollector(TestCase):
+    """
+    Tests for `collect_warnings` helper.
+    """
+
+    def test_no_warnings(self):
+        """
+        No error is raised if no warning is raised inside the context
+        manager.
+        """
+        with testing.collect_warnings():
+            """
+            No warning emitted here.
+            """
+
+    def test_fail_on_unchecked_warnings(self):
+        """
+        No error is raised if no warning is raised inside the context
+        manager.
+        """
+        with self.assertRaises(AssertionError) as context:
+            with testing.collect_warnings():
+                warnings.warn("A warning that is not checked.")
+
+        self.assertTrue(context.exception.args[0].startswith(
+            'Warnings queue was not completely checked.'))
+
+    def test_expect_warning(self):
+        """
+        The context manager returns a helper that can be used to
+        assert the emitted warnings.
+        """
+        with testing.collect_warnings() as collector:
+            warnings.warn("A warning that is checked.", UserWarning)
+
+            collector.expect(
+                'A warning that is checked.', category=UserWarning)
+
+    def test_expect_warning_unchecked(self):
+        """
+        When an emitted warning is left unchecked, an AssertionError is
+        raised when the context exists.
+        """
+        with self.assertRaises(AssertionError) as context:
+            with testing.collect_warnings() as collector:
+                warnings.warn("A warning that is checked.", UserWarning)
+                warnings.warn("A warning that is checked.", UserWarning)
+
+                collector.expect(
+                    'A warning that is checked.', category=UserWarning)
+
+        self.assertTrue(context.exception.args[0].startswith(
+            'Warnings queue was not completely checked.'))
+
+    def test_expect_regret(self):
+        """
+        It provide a helper to expect a warning raised by regret.
+        """
+        deprecated = regret.callable(version="1.2.0")(calculate)
+
+        with testing.collect_warnings() as collector:
+            deprecated()
+
+            collector.expect_regret('calculate')
+
+    def test_expect_regret_with_replacement(self):
+        """
+        It provide a helper to expect a warning raised by regret.
+        """
+        deprecated = regret.callable(
+            version="1.2.0",
+            replacement=replacement_calculate,
+            )(calculate)
+
+        with testing.collect_warnings() as collector:
+            deprecated()
+
+            collector.expect_regret(
+                'calculate', replacement='replacement_calculate')
+
+    def test_expect_regret_source(self):
+        """
+        It provide a helper to expect a warning raised by regret from
+        a specific source file.
+        """
+        deprecated = regret.callable(version="1.2.0")(calculate)
+
+        with testing.collect_warnings() as collector:
+            deprecated()
+
+            collector.expect_regret('calculate', source='test_testing')
+
+    def test_expect_regret_source_mismatch(self):
+        """
+        It raised AssertionError when the expected source is different than
+        the source associated with the regret.
+        """
+        deprecated = regret.callable(version="1.2.0")(calculate)
+
+        with self.assertRaises(AssertionError) as context:
+            with testing.collect_warnings() as collector:
+                deprecated()
+
+                collector.expect_regret('calculate', source='bad_source')
+
+        self.assertTrue(context.exception.args[0].startswith(
+            'Expecting warning from path ending with "bad_source.py". Got '))
