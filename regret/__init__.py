8d34cb9d45d3d3687069c1de8ee22f7e73791fff
Julian Berman <Julian@GrayVines.com>
2023-05-11 17:40:44 -0700

    Man do I hate all this nonsense.

--- regret/__init__.py
+++ regret/__init__.py
@@ -1,10 +1,8 @@
 """
 You made a thing, but now you wish it'd go away... Deprecations, a love story.
 """
-from regret._api import (  # type: ignore[reportPrivateUsage]  # noqa: E501
-    _DEPRECATOR,
-    Deprecator,
-)
+from regret._api import _DEPRECATOR  # type: ignore[reportPrivateUsage]
+from regret._api import Deprecator
 
 callable = _DEPRECATOR.callable
 inheritance = _DEPRECATOR.inheritance
